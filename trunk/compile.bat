35bc67126661a54291bbf8d873127e28067a5810
jadothebest <jadothebest@565685f7-e3c2-c7c9-1ae8-c8f3f43742d0>
2010-03-22 09:53:05 +0000

    deleted all

--- trunk/compile.bat
+++
@@ -1,6 +0,0 @@
-javac -extdirs lib/ core/*.java
-javac -extdirs lib/ movement/*.java
-javac -extdirs lib/ report/*.java
-javac -extdirs lib/ routing/*.java
-javac -extdirs lib/ gui/*.java
-javac -extdirs lib/ input/*.java
