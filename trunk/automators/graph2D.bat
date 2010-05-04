8a47279f8852737fbc29f329e541032e01320f5b
jadothebest <jadothebest@565685f7-e3c2-c7c9-1ae8-c8f3f43742d0>
2010-05-04 18:45:54 +0000

    updated automators

--- trunk/automators/graph2D.bat
+++ trunk/automators/graph2D.bat
@@ -8,6 +8,7 @@ FOR /F "delims=" %%i IN (vars) do set VAR= %%i
 FOR %%r in (SW,AFS, AFSnW) do call per_router.bat %%r
 
 gawk -f x-axis_interpreter.awk %VAR% vars  >x_axis
-FOR /F "delims=" %%i IN (x_axis) DO  gnuplot -e "set term postscript eps enhanced; set output '../graphs/%1.eps';set xlabel '%%i'; set ylabel '%2';plot '../values/SW_latency' using 1:2 with lines"
+FOR /F "delims=" %%i IN (x_axis) DO set X_label= %%i
+FOR %%y in (delivery_prob,overhead_ratio, average_energy,dead_nodes,latency,av_latency) do gnuplot -e "set term postscript eps enhanced; set output '../graphs/%1_%%y.eps';set xlabel '%X_label%'; set ylabel '%%y';plot '../values/SW_%%y' using 1:2 with lines;plot '../values/AFS_%%y' using 1:2 with lines;plot '../values/AFSnW_%%y' using 1:2 with lines"
 REM ;plot 'FSnWnA_values' using 1:2 with lines
 erase vars, vars_all, x_axis
