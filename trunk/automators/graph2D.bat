e7ad40eac9e97ffb4956ca5a50bb027441293976
jadothebest <jadothebest@565685f7-e3c2-c7c9-1ae8-c8f3f43742d0>
2010-05-05 13:31:07 +0000

    a bug fix in automators. since we are drawing all 3 routers on same graph, we have to have a common reference of cum_prob. I was finding minimum per routing scheme. Now I fixed something for it to be global per 3 router schemes

--- trunk/automators/graph2D.bat
+++ trunk/automators/graph2D.bat
@@ -7,6 +7,15 @@ FOR /F "delims=" %%i IN (vars) do set VAR= %%i
 
 FOR %%r in (SW,AFS, AFSnW) do call per_router.bat %%r
 
+erase min,temp
+FOR %%r in (SW,AFS, AFSnW) do FOR /F "delims=" %%f IN (%%r_file) do gawk -f max_cum_prob.awk %%f >> temp
+gawk -f get_min.awk temp >min
+FOR /F "delims=" %%m IN (min) do set MIN_VALUE=%%m
+FOR %%r in (SW,AFS, AFSnW) do FOR /F "delims=" %%f IN (%%r_file) DO FOR %%m in (latency,av_latency) do gawk -f latency.awk metric="%%m" min=%MIN_VALUE% %%f >> ..\values\%%r_%%m_1
+FOR %%r in (SW,AFS, AFSnW) do FOR %%m in (latency,av_latency) do gawk -f sort.awk  ..\values\%%r_%%m_1 > ..\values\%%r_%%m
+FOR %%r in (SW,AFS, AFSnW) do FOR %%m in (latency,av_latency) do erase  ..\values\%%r_%%m_1, %%r_file
+erase min,temp
+
 gawk -f x-axis_interpreter.awk %VAR% vars  >x_axis
 FOR /F "delims=" %%i IN (x_axis) DO set X_label= %%i
 FOR %%y in (delivery_prob,overhead_ratio, average_energy,dead_nodes,latency,av_latency) do gnuplot -e "set term postscript eps enhanced; set output '../graphs/%1_%%y.eps';set xlabel '%X_label%'; set ylabel '%%y';plot '../values/SW_%%y' using 1:2 with lines;plot '../values/AFS_%%y' using 1:2 with lines;plot '../values/AFSnW_%%y' using 1:2 with lines"
--- trunk/automators/per_router.bat
+++ trunk/automators/per_router.bat
@@ -1,4 +1,4 @@
-erase temp_files, %1_file,temp,min
+erase temp_files, %1_file
 for /f %%a IN ('dir ..\reports\%1_*.txt /B') do echo "%%~na" >>temp_files
 REM dir ..\reports\%1_*.txt /B > temp_files
 gawk -f name_decoder.awk %VAR% temp_files >> %1_file
@@ -6,10 +6,4 @@ FOR %%m in (delivery_prob,overhead_ratio, average_energy,dead_nodes,latency,av_l
 FOR /F "delims=" %%f IN (%1_file) DO FOR %%m in (delivery_prob,overhead_ratio, average_energy,dead_nodes) do gawk -f average.awk metric="%%m" %%f >> ..\values\%1_%%m_1
 FOR %%m in (delivery_prob,overhead_ratio, average_energy,dead_nodes) do gawk -f sort.awk  ..\values\%1_%%m_1 > ..\values\%1_%%m
 FOR %%m in (delivery_prob,overhead_ratio, average_energy,dead_nodes) do erase  ..\values\%1_%%m_1
-FOR /F "delims=" %%f IN (%1_file) do gawk -f max_cum_prob.awk %%f >> temp
-gawk -f get_min.awk temp >min
-FOR /F "delims=" %%m IN (min) do set MIN_VALUE=%%m
-FOR /F "delims=" %%f IN (%1_file) DO FOR %%m in (latency,av_latency) do gawk -f latency.awk metric="%%m" min=%MIN_VALUE% %%f >> ..\values\%1_%%m_1
-FOR %%m in (latency,av_latency) do gawk -f sort.awk  ..\values\%1_%%m_1 > ..\values\%1_%%m
-FOR %%m in (latency,av_latency) do erase  ..\values\%1_%%m_1,..\values\%1_%%m
-erase temp_files, %1_file,temp,min
+erase temp_files
